1892d5b4aff877bf6a9278fc52c55ecbdee752a9
DESKTOP-6APP6KP\APC <cuongkoi1406@gmail.com>
2025-12-04 15:00:13 +0700

    update

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,10 +1,13 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_JSON);
-
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_JSON);
+
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      ...serviceAccount,
+      private_key: serviceAccount.private_key.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
