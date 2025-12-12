21e36a3acee6dd39200610d5f43d14e97168532e
DESKTOP-6APP6KP\APC <cuongkoi1406@gmail.com>
2025-12-12 11:36:06 +0700

    update

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,18 +1,25 @@
 import { baseResponse } from "../utils/response.helper.js";
-import { encryptText } from "../utils/encryption.js";
+import { decryptText, encryptText } from "../utils/encryption.js";
 import { MessageModel } from "../models/message.model.js";
 
 export const MessageController = {
   async sendMessage(req, res) {
     try {
-      const { room_id, receiver_id, content_text, content_type, reply_id, name_file } = req.body;
+      const {
+        room_id,
+        receiver_id,
+        content_text,
+        content_type,
+        reply_id,
+        name_file,
+      } = req.body;
       const sender_id = req.user.id;
 
       if (!room_id || !receiver_id || !content_type) {
         return baseResponse(res, {
           code: 400,
           status: false,
-          message: "Thiếu dữ liệu gửi tin nhắn"
+          message: "Thiếu dữ liệu gửi tin nhắn",
         });
       }
 
@@ -28,7 +35,7 @@ export const MessageController = {
         reply_id: reply_id || null,
         sender_id,
         receiver_id,
-        name_file: name_file || null
+        name_file: name_file || null,
       };
 
       await MessageModel.create(newMessage);
@@ -37,14 +44,14 @@ export const MessageController = {
         code: 200,
         status: true,
         message: "Gửi tin nhắn thành công",
-        data: { message_id: newMessage.message_id }
+        data: { message_id: newMessage.message_id },
       });
     } catch (err) {
       console.error(err);
       return baseResponse(res, {
         code: 500,
         status: false,
-        message: "Lỗi gửi tin nhắn"
+        message: "Lỗi gửi tin nhắn",
       });
     }
   },
@@ -55,19 +62,35 @@ export const MessageController = {
 
       const messages = await MessageModel.getMessages(room_id);
 
+      const finalMessages = messages.map((msg) => ({
+        ...msg,
+        content_text: msg.content_text_encrypt
+          ? safeDecrypt(msg.content_text_encrypt)
+          : null,
+      }));
+
       return baseResponse(res, {
         code: 200,
         status: true,
         message: "Lấy danh sách tin nhắn thành công",
-        data: messages
+        data: finalMessages,
       });
     } catch (err) {
       console.error(err);
       return baseResponse(res, {
         code: 500,
         status: false,
-        message: "Lỗi lấy danh sách tin nhắn"
+        message: "Lỗi lấy danh sách tin nhắn",
       });
     }
-  }
+  },
 };
+// Hàm giải mã an toàn
+function safeDecrypt(text) {
+  try {
+    return decryptText(text);
+  } catch (err) {
+    console.error("Decrypt error:", err);
+    return null;
+  }
+}
